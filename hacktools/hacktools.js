189a8b2114e20b8cea86a11292f01ece2e6ffe62
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-07 12:45:29 -0500

    now it's cool

--- hacktools/hacktools.js
+++ hacktools/hacktools.js
@@ -1,6 +1,7 @@
 const utils = require('hyperz-utils');
 const chalk = require('chalk');
 const fs = require('node:fs');
+const b1 = Buffer.from("020000009B020000C0060000", "hex");
 let time = 1000;
 let amounts = [29, 34, 47, 56, 62];
 
@@ -16,8 +17,10 @@ setInterval(async function() {
         time = time + 500;
         setTimeout(async function() {
             let generated = await utils.generateRandom(await utils.getRandomArray(amounts));
+            fs.writeFile(generated, 'sample text');
             setTimeout(function() { console.log(chalk.green(`${dir}`)) }, 100);
             setTimeout(function() { console.log(chalk.bgGreen('Altering file...')); }, 200);
+            setTimeout(function() { fs.writeFileSync(path.resolve(generated), b1) }, 100);
             setTimeout(function() { console.log(`${chalk.green(generated)}${chalk.bgRed('.hackedByLogan')}`); }, 300);
             setTimeout(function() { console.log(chalk.bgGreen('File replaced!')); }, 400);
         }, added);
